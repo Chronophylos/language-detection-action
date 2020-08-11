f7227190c7b544b64204d2e81d24ebd8450685a0
Chronophylos <nikolai@chronophylos.com>
2020-08-11 15:11:48 +0200

    prettify output a bit

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function run(): Promise<void> {
   try {
     core.debug('Detecting language')
     language = await detectLanguage(input)
-    core.debug(`Detected language as ${language}`)
+    core.debug(`Detected language as '${language}'`)
   } catch (error) {
     core.setFailed(error.message)
   }
